b7a0c0a7a14fe44c9277b0add4097da38df9e712
Santiago Barletta <santiagobarletta@outlook.com>
2024-12-09 13:12:13 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Configuración de CORS
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "https://cloneslack-rho.vercel.app", 
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true, 
