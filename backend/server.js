cfb122f2a5aaf4cf81dcf12d3fe6598a9f425e44
Santiago Barletta <santiagobarletta@outlook.com>
2024-12-16 13:36:53 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,8 @@ app.use(customCorsMiddleware);
 
 app.use(cors()); 
 
-app.use(express.json());
+app.use(express.json({ limit: '5mb' })); 
+app.use(express.urlencoded({ limit: '5mb', extended: true }));
 
 app.use("/api/auth", authRouter);
 app.use("/api/workspaces", workspacesRouter);
