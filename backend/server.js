d3644bed6011670d5e10e27ac2529da542d03a78
Santiago Barletta <santiagobarletta@outlook.com>
2024-12-17 08:19:12 -0300

    Merge branch 'main' of https://github.com/SantiagoBarletta/CLONESLACK

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,11 @@ import privateMessagesRouter from "./routes/privateMessages.routes.js";
 
 const app = express();
 
-app.use(customCorsMiddleware);
+
 
 app.use(cors()); 
 
-app.use(express.json());
+app.use(express.json({limit: '1mb'}));
 
 app.use("/api/auth", authRouter);
 app.use("/api/workspaces", workspacesRouter);
